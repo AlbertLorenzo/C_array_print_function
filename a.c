52f728cf18f331b2222a53d30c2c7144905c0a13
Albert Lorenzo Segarra <albertlorenzofc@gmail.com>
2021-11-11 16:30:35 +0100

    Update a.c

--- a.c
+++ a.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 
-void simple_array_print(int *ptr, int size)
+void fnt(int *p, int s)
 {
     short i = 0;
-    while (i < (size / sizeof(ptr[0])))
+    const int n = (s / sizeof((*p)));
+    while (i < n)
     {
-        printf("Addres: 0x%p, Value: %d, index: %d\n", ptr, (*ptr), i);
+        printf("Addres: %p, value: %d, index: %d\n", p, (*p), i);
         i++;
-        ptr += 1;
+        p += 1;
     }
 }
 
 int main(void)
 {
     int a = 44, b = 12, c = 36, d = 94, e = 75;
-    simple_array_print(&a, sizeof(a) * 5);
+    printf("%p\n", &a);
+    printf("%p\n", &b);
+    printf("%p\n", &c);
+    printf("%p\n", &d);
+    printf("%p\n", &e);
+    printf("%p\n", &a);
+    printf("%p\n", &b);
+    printf("%p\n", &c);
+    printf("%p\n", &d);
+    printf("%p\n", &e);
+    fnt(&a, sizeof(int) * 5);
     return 0;
 }
